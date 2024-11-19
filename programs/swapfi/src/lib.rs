ac95bb19ae17f45d6823f11fe0c510d34e9f3e5b
Amit Kumar <amitkcodes@gmail.com>
2024-11-20 01:39:14 +0530

    'cargo fmt' run

--- programs/swapfi/src/lib.rs
+++ programs/swapfi/src/lib.rs
@@ -7,7 +7,7 @@ use anchor_lang::prelude::*;
 
 pub use constants::*;
 pub use instructions::*;
-pub use state::*; 
+pub use state::*;
 
 declare_id!("36Cj3imCYw1mKJV7FXwwohDSUWGHn2KL1MJXmSYGTqoz");
 
@@ -15,7 +15,12 @@ declare_id!("36Cj3imCYw1mKJV7FXwwohDSUWGHn2KL1MJXmSYGTqoz");
 pub mod swapfi {
     use super::*;
 
-    pub fn create_swap_offer(ctx: Context<CreateOffer>, id: u64, provided_token_amount: u64, requested_token_amount: u64) -> Result<()>{
+    pub fn create_swap_offer(
+        ctx: Context<CreateOffer>,
+        id: u64,
+        provided_token_amount: u64,
+        requested_token_amount: u64,
+    ) -> Result<()> {
         instructions::lock_tokens_to_vault(&ctx, provided_token_amount)?;
         instructions::save_offer_on_chain(ctx, id, requested_token_amount)?;
         Ok(())
